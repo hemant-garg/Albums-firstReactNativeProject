db1da03d54983728ec2c299145d3a27295eca6af
Hemant Garg <hmtcool4u@gmail.com>
2018-09-25 22:44:15 +0530

    files added

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { AppRegistry, View } from "react-native";
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
+
+const App = () => {
+	return (
+		<View style={{ flex: 1 }}>
+			<Header title={"Albums"} />
+			<AlbumList />
+		</View>
+	);
+};
+
+AppRegistry.registerComponent("firstReactNative", () => App);
